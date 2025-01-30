44bb188fb1291acd704d3284ae9553ab13dd4486
Tanmay Singh Thakur <itsthakurtanmay@gmail.com>
2025-01-30 12:02:35 +0530

    string codes

---
+++ DSA_2A(Honors)/String/StringClassAndObject.java
@@ -0,0 +1,12 @@
+public class StringClassAndObject {
+
+    public static void main(String[] args) {
+        // class
+        System.out.println(String.class);
+
+        //object;
+        String s = "hello java";
+        System.out.println(s instanceof Object);
+        
+    }
+}
