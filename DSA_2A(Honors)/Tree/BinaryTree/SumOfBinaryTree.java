dc457b19682679f64f85767f394448c07d17837c
Tanmay Singh Thakur <itsthakurtanmay@gmail.com>
2025-04-15 10:54:50 +0530

    15-04-2025

--- DSA_2A(Honors)/Tree/BinaryTree/SumOfBinaryTree.java
+++ DSA_2A(Honors)/Tree/BinaryTree/SumOfBinaryTree.java
@@ -1,3 +1,24 @@
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class SumOfBinaryTree {
-    
+
+    static int sumBT(Node root) {
+        // Your code here
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        int sum = 0;
+        while (!q.isEmpty()) {
+            Node temp = q.poll();
+            sum += temp.data;
+            if (temp.left != null) {
+                q.add(temp.left);
+            }
+            if (temp.right != null) {
+                q.add(temp.right);
+            }
+        }
+        return sum;
+    }
 }
