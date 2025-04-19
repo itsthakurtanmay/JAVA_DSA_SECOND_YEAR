709a3e2b50e711df9ae7412645281b62470a6223
Tanmay Singh Thakur <itsthakurtanmay@gmail.com>
2025-04-19 10:56:54 +0530

    forgot to save Stock span problem

---
+++ DSA_2B/Stack Questions/Monotonic Stack/StockSpanProblem.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
+class StockSpanProblem {
+    public ArrayList<Integer> calculateSpan(int[] arr) {
+        ArrayList<Integer> span = new ArrayList<>();
+        Stack<Integer> st = new Stack<>();
+        
+        for (int i = 0; i < arr.length; i++) {
+            int days = 1;
+            while (!st.isEmpty() && arr[st.peek()] <= arr[i]) {
+                days += span.get(st.pop());
+            }
+            span.add(days);
+            st.push(i);
+        }
+        return span;
+    }
+}
